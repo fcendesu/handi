e3e0deb7a6148aeb8d3c61dc023aeb7aa70827f3
fcendesu <furkan.ckmk@outlook.com>
2025-02-06 22:35:24 +0200

    first commit

---
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            "name" => "required|min:4|max:255",
+            "email" => "required|email|unique:users",
+            "password" => "required|min:4|max:255",
+        ]);
+
+        $userData = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+
+        $user = User::create($userData);
+        $token = $user->createToken("auth_token");
+
+        return response(
+            [
+                'name' => $user,
+                'token' => $token->plainTextToken,
+            ],
+            201,
+        );
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required|min:4|max:255",
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'Invalid credentials',
+            ], 422);
+        }
+
+        $token = $user->createToken("auth_token");
+
+        return response([
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+
+        $request->user()->tokens()->delete();
+
+        return response([
+            'message' => 'Logged out successfully',
+        ], 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//   return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::get('/test', function () {
+
+});
+
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::post('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
